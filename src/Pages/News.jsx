7b0ab8f5214944ec32a769e20c62663feb443d4b
Danya <daniilerkesov122@gmail.com>
2024-05-30 16:32:53 +0300

    Development of a news block. Creating components: News Card, Show More, Statistic Card

--- src/Pages/News.jsx
+++ src/Pages/News.jsx
@@ -1,29 +1,31 @@
 import React from "react";
+
 import { useGetLastNewsQuery } from "../Redux/CryptoCoin/cryptoNews.api";
 
+import { NewsCard } from "../Component/NewsCard/NewsCard";
+
 export const News = () => {
-    const { isLoading, isError, data: news } = useGetLastNewsQuery();
+    const { isLoading, isError, currentData: news } = useGetLastNewsQuery({});
 
-    console.log(news);
-
-    if (isError) {
-        <h1>Try later</h1>;
-    }
+    if (isError && !news)
+        return (
+            <div>
+                <h1>Data missing. Try later!</h1>
+            </div>
+        );
 
     return (
         <>
-            <div className="grid gap-2 grid-cols-4">
-                {isLoading && <h1>Loadind news...</h1>}
-                {news?.map((newsItem) => (
-                    <div className="w-full h-96  rounded-xl border  shadow-xl overflow-hidden p-5 transform transition-transform hover:scale-105">
-                        <img
-                            src={newsItem?.image_url}
-                            alt="News image"
-                            className="h-full w-full"
+            <div className="grid gap-6 grid-cols-4 p-4">
+                {isLoading && <h1>Loading news...</h1>}
+                {news &&
+                    news.map((newsItem, i) => (
+                        <NewsCard
+                            resource={newsItem}
+                            index={i}
+                            key={newsItem?.article_id}
                         />
-                        <div className="flex"></div>
-                    </div>
-                ))}
+                    ))}
             </div>
         </>
     );
